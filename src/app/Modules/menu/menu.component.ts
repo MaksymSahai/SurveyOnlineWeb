37268ebaf6226056baf7f0027bd12e6c5b24dde7
Oleksii Vylehzhanin <o.vylehzhanin@easygenerator.com>
2019-12-02 16:56:06 +0200

    wip: refactor navigation bar

--- src/app/Modules/menu/menu.component.ts
+++ src/app/Modules/menu/menu.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  navItems = [
+    { path: '/', label: 'Home' },
+    { path: '/about-us', label: 'About us' },
+    { path: '/how-it', label: 'How it...' },
+    { path: '/faq', label: 'FAQ' },
+    { path: '/feedback', label: 'Contact us' }
+  ];
 
   constructor() { }
 
